55a343a921c3d0f1195e12978c085415d2225321
cs-308-2023 <b20241@students.iitmandi.ac.in>
2023-08-18 21:29:14 +0530

    Factorial.cpp changes

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,11 @@
 #include "functions.h"
 
 int factorial(int n){
+    // check for negative numbers
+    if (n<0){
+       cout<<"Error: Please Enter a number greater than zero."<<endl;
+       exit(0);
+     }
     if(n!=1){
 	return(n * factorial(n-1));
     }

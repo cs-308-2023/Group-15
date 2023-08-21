62dcbacd6127005b14b5d5693f745e01fb65f6b7
Avni Mittal <avni.mittal2002@gmail.com>
2023-08-21 14:43:43 +0530

    negetive num bug fixed

--- factorial.cpp
+++ factorial.cpp
@@ -4,8 +4,7 @@
 int factorial(int n){
     // check for negative numbers
     if (n<0){
-       printf("Error: Please Enter a number greater than zero.\n");
-       exit(0);
+       return -1;
      }
     if(n!=1){
 	return(n * factorial(n-1));
